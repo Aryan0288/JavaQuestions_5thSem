c90195e07e1ba4a7058899e1deab9c60ef05c798
Aryan <aryansharma31658@gmail.com>
2023-11-17 09:35:52 +0530

    do some changes in findString

--- FindString.java
+++ FindString.java
@@ -1,9 +1,16 @@
 public class FindString {
     public static void main(String[] args) {
-        String s="aaaaaabc";
-        String ptrn="abc";
-        boolean n=s.contains(ptrn);
-        int i=s.indexOf(ptrn);
-        System.out.println(n+" "+i);
+        // String s="aaaaaabc";
+        // String ptrn="abc";
+        // boolean n=s.contains(ptrn);
+        // int i=s.indexOf(ptrn);
+        // System.out.println(n+" "+i);
+
+
+        // KMP alogrithm -> Knuth–Morris–Pratt algorithm
+
+        
+
+
     }
 }
