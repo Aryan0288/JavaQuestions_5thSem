b018a5914132c1520a5ad2355d74aa2138a4a650
Aryan <aryansharma31658@gmail.com>
2023-11-23 10:41:55 +0530

    some changes and create new file

--- FindString.java
+++ FindString.java
@@ -9,6 +9,16 @@ public class FindString {
 
         // KMP alogrithm -> Knuth–Morris–Pratt algorithm
 
+        String s= "abcd";
+        String s1= "";
+        StringBuilder sb=new StringBuilder(s);
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            s1=c+s1;
+        }
+        System.out.println(s1);
+
         
 
 
---
+++ prefix.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.*;
+public class prefix {
+    public static void main(String[] args) {
+        String str="aabcaaabcaabc";
+        int n=str.length();
+
+        int z[] =new int[n];
+        int left=0;
+        int right=0;
+        for(int i=0;i<n;i++){
+            if(i>right){
+                left=i;
+                right=i;
+                while(str.charAt(right)==str.charAt(right-left)){
+                    right++;
+                }
+                right--;
+                z[i]=right-left+1;
+            }else{
+                if(z[i-left]<z[right-i+1]){
+                    z[i]=z[i-left];
+                }else{
+                    left=i;
+                    while(right<n && str.charAt(right)==str.charAt(right-left)){
+                        right++;
+                    }
+                    right--;
+                    z[i]=right-left+1;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(z));
+    }
+}
